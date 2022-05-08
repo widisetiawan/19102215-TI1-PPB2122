038d4e178d235ff66bcb8d5283ea3f694c10e140
widisetiawan <102368204+widisetiawan@users.noreply.github.com>
2022-05-09 06:13:19 +0700

    tugas 5

---
+++ praktikum5/src/main/java/com/widi_19102215/praktikum5/Practice5ReadDataActivity.kt
@@ -0,0 +1,19 @@
+package com.widi_19102215.praktikum5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_practice5_read_data.*
+
+class Practice5ReadDataActivity : AppCompatActivity() {
+    companion object {
+        const val EXTRA_PRODI = "extra_prodi"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_practice5_read_data)
+        val prodi = intent.getStringExtra(EXTRA_PRODI)
+        lblProdiSaya.text = "Prodi Saya Adalah $prodi"
+
+    }
+}
